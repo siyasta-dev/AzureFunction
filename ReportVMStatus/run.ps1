4b2fb3a10ea73a225c5e1b86ca2eb9d65c9078c0
Vivek Saurabh <saurabhvivek30@gmail.com>
2023-04-22 15:47:42 +0530

    compare the result

--- ReportVMStatus/run.ps1
+++ ReportVMStatus/run.ps1
@@ -3,18 +3,31 @@ param($Timer)
 # Set Up the query for Resource graph
 $Query = @"
 resources
-| where type == "microsoft.storage/storageaccounts" | project name, type, kind
+| where type == "microsoft.storage/storageaccounts" | project id, name, type, kind
 "@
 
 $StorageAccountResourceGroup = "sqlbrains_group"    #Resource Group Name
 $StorageAccountName = "vmrestartdata"               #Storage Account Name
 $StorageTableName = "restartdata"                   #Storage Table Name
 $StorageTablePartitionKey = 'VMPartition'           #Partion Name (Can be anything)
+$PropertyToCompare = "kind"
+$PrimaryPropertyKey = "id"
 
 $Result = Search-AzGraph -Query $Query
 
 $storageAccount = Get-AzStorageAccount -Name $StorageAccountName -ResourceGroupName $StorageAccountResourceGroup
 $storageTable = Get-AzStorageTable -Context $storageAccount.Context -Name $StorageTableName
+
+$UpdatedResult = @()
+foreach($element in $Result){
+    $oldElement = Get-AzTableRow -Table $storageTable.CloudTable `
+        -CustomFilter "$($PrimaryPropertyKey) eq '$($element.$PrimaryPropertyKey)'" -Top 1
+
+    if($oldElement.$PropertyToCompare -ne $element.$PropertyToCompare)
+    {
+        $UpdatedResult += $element
+    }
+}
 
 Get-AzTableRow -Table $storageTable.CloudTable | Remove-AzTableRow -Table $storageTable.CloudTable
 
@@ -25,10 +38,11 @@ foreach($element in $Result)
     {
         $hashtable[$property] = $element.$property
     }
+
     Add-AzTableRow -Table $storageTable.CloudTable -property $hashtable -partitionKey $StorageTablePartitionKey -rowkey ([guid]::NewGuid().tostring())
 }
 
-$Body = @{data = (($Result | ConvertTo-Html -Fragment) -join "")} | ConvertTo-Json
+$Body = @{data = (($UpdatedResult | ConvertTo-Html -Fragment) -join "")} | ConvertTo-Json
 
 Invoke-RestMethod -Method Post -Uri $env:LOGIC_APP_URL -Body $Body -ContentType "application/json"
 
